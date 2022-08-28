02c1410f7b9fd6e46c795927ad9a7a7d1c80bf87
CorrineMuriuki <Corrine.Muriuki@student.moringaschool.com>
2022-08-28 04:30:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+
+  // I used textContent to get the element's text content
+  let text = document.getElementById('text').textContent = "This is really cool!";
+  });
